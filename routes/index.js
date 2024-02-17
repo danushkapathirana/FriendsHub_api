4325cf475258570f05ca3fbbbba51d3e515317ed
Danushka Pathirana <119834779+danushkapathirana@users.noreply.github.com>
2024-02-18 00:31:31 +0530

    develop basic route paths

---
+++ routes/index.js
@@ -0,0 +1,10 @@
+const express = require("express")
+const router = express.Router()
+
+const userRoute = require("./user")
+const profileRoute = require("./profile")
+const postRoute = require("./post")
+
+router.use("/user", userRoute)
+router.use("/profile", profileRoute)
+router.use("/post", postRoute)
