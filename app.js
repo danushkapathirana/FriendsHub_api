b96a684799c65200056fe4a8ddc62b7a93c5dacc
Danushka Pathirana <119834779+danushkapathirana@users.noreply.github.com>
2024-05-16 00:16:30 +0530

    develop database connection

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
 const express = require("express")
 require("dotenv").config()
+const config = require("config")
+const { default: mongoose } = require("mongoose")
+
+const databaseConnection = async() => {
+    try {
+        await mongoose.connect(config.get("database_url"))
+        console.log("connected to the database")
+    }
+    catch(error) {
+        console.log(error)
+    }
+}
 
 const app = express()
+databaseConnection()
 
 app.listen(process.env.PORT || 9000, () => {
     console.log("server started")
@@ -12,4 +25,5 @@ app.listen(process.env.PORT || 9000, () => {
  * libraries
  * 
  * npm install mongoose
+ * npm install config
  */
